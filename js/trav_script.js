8a453062e23269188523451778b0c55ae78339c5
rajsaha <pontypagla@live.com>
2017-02-03 19:39:06 +0800

    Minor update - Added User Image
    User profile picture is now shown. Added hover effect to controls.

--- js/trav_script.js
+++ js/trav_script.js
@@ -3,21 +3,14 @@ var json_data = "js/data.json";
 $.getJSON(json_data, function(data) {
   var output = '';
 
-  // $.each(data, function(key,val) {
-  //   output += '<span id="travel-type">' + key.location + '</span>';
-  //   output += '<span id="user-photo">' + key.user_name + '</span>';
-  //   console.log(key.location);
-  //   console.log(key.user_name);
-  // });
-
   $.each(data.cards, function(k,v){
       output = '<div class="row"><div class="col-md-12 text-center travscapade"><div>';
       output += '<img src="' + v.url + '"/>';
       output += '</div><div><p class="text-center"><span class="glyphicon glyphicon-heart" aria-hidden="true" id="heart"></span><span class="glyphicon glyphicon-plus" aria-hidden="true" id="plus"></span><br /></p></div><div id="details">';
       output += '<span id="travel-type">' + v.interests + '</span>';
       output += '<span id="user-photo">' + v.user_name + '</span>';
+      output += '<span><img src="' + v.user_profile_pic + '"/></span>';
       output += '</div></div></div>';
-      console.log(k+' '+v);
       $('#update').append(output);
   });
 }); //get json data from URL
