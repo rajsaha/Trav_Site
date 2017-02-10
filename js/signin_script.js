7593d6dc6923a3e6b3afb69c5bb676ac3c70e3bc
rajsaha <pontypagla@live.com>
2017-02-10 11:13:35 +0800

    Added Sign In Page

---
+++ js/signin_script.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  setTimeout(function() {
+    $("#hill1").removeClass("border-no-fill");
+  },0);
+
+  setTimeout(function() {
+    $("#hill2").removeClass("border-no-fill");
+  },500);
+
+  setTimeout(function() {
+    $("#mountain").removeClass("border-no-fill");
+  },800);
+
+  setTimeout(function() {
+    $("#rect-bg").removeClass("border-no-fill");
+  },1200);
+
+  setTimeout(function() {
+    $("#sun2").removeClass("border-no-fill");
+  },1400);
+
+  setTimeout(function() {
+    $("#sun1").removeClass("border-no-fill");
+  },2000);
+});
--- js/trav_script.js
+++ js/trav_script.js
@@ -6,9 +6,11 @@ $(document).ready(function() {
 
         $.each(data.cards, function(k, v) {
             output = '<div class="row"><div class="col-md-12 text-center travscapade"><div class="user-content">';
-            output += '<img src="' + v.url + '" id="'+ v._id + '" class="travimage"/>';
+            output += '<img src="' + v.url + '" id="'+ v._id + '" class="travimage" data-name="travimage"/>';
             output += '<div class="location-text"><p class="'+ v._id +'">'+ v.location +'</p></div>';
-            output += '</div><div><p class="text-center"><button class="glyphicon glyphicon-heart heart" aria-hidden="true" value="'+ v._id +'"></button><button class="glyphicon glyphicon-plus" aria-hidden="true" id="plus"></button><br /></p></div><div id="details">';
+            output += '</div><div><p class="text-center">';
+            output += '<button class="glyphicon glyphicon-heart heart button-override" aria-hidden="true" value="'+ v._id +'"></button>';
+            output += '<button class="glyphicon glyphicon-plus button-override" aria-hidden="true" id="plus"></button><br /></p></div><div id="details">';
             output += '<a id="travel-type">' + v.interests + '</a>';
             output += '<a id="user-photo">' + v.user_name + '</a>';
             output += '<a><img src="' + v.user_profile_pic + '"/></a>';
@@ -21,35 +23,52 @@ $(document).ready(function() {
       var image_id = $(this).attr("value");
       var interest = $("#travel-type." + image_id).text();
       var location = $("."+image_id).text();
-      console.log(location);
       $(this).css("color","red");
       $(this).css("-webkit-text-stroke", "0px");
       $("#"+image_id).css("filter", "grayscale(100%)");
       $("#"+image_id).css("transition", "filter 0.5s ease-in-out");
-      $("."+ image_id).addClass("important-opacity-1");
-      //$("#travel-type." + image_id).text(interest + " in " + location);
-      console.log(image_id);
-      var output_css = $("."+ image_id).css("opacity","0.0");
-      //console.log(output_css);
+      $("."+ image_id).addClass("important-display-on");
+
+      setTimeout(function() {
+        $("."+ image_id).addClass("important-opacity-1");
+      },200);
 
       setTimeout(function() {
         $("#"+ image_id).css("filter", "grayscale(0%)");
         $("."+ image_id).addClass("important-opacity-0");
       }, 2000);
-    });
 
-    $(document).on("dblclick", ".travimage" , function() {
+      setTimeout(function() {
+        $("."+ image_id).addClass("important-display-off");
+        $("."+ image_id).removeClass("important-opacity-0");
+        // $("."+ image_id).removeClass("important-opacity-0");
+        // $("."+ image_id).removeClass("important-opacity-1");
+      },2500);
+    }); //like button (heart) function
+
+    $(document).on("dblclick",".travimage", function() {
       var image_id = $(this).attr("id");
       console.log(image_id);
       $("input[value=" + image_id + "]").css("color","red");
       $("input[value=" + image_id + "]").css("-webkit-text-stroke", "0px");
       $(this).css("filter", "grayscale(100%)");
       $(this).css("transition", "filter 0.5s ease-in-out");
-      $("."+ image_id).addClass("important-opacity-1");
+      $("."+ image_id).addClass("important-display-on");
+
+      setTimeout(function() {
+        $("."+ image_id).addClass("important-opacity-1");
+      },200);
 
       setTimeout(function() {
         $("#"+image_id).css("filter", "grayscale(0%)");
         $("."+ image_id).addClass("important-opacity-0");
       }, 2000);
-    });
+
+      setTimeout(function() {
+        $("."+ image_id).addClass("important-display-off");
+        $("."+ image_id).removeClass("important-opacity-0");
+        // $("."+ image_id).removeClass("important-opacity-0");
+        // $("."+ image_id).removeClass("important-opacity-1");
+      },2500);
+    }); //double-click on image to like function
 }); //document ready
