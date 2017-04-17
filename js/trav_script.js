333cad1d7cd0ab446ee36f3d2d7b7314193cb2d4
Hassan <hassan.ali.askari@gmail.com>
2017-04-17 13:24:18 +0800

    Added fething data from backend

--- js/trav_script.js
+++ js/trav_script.js
@@ -1,5 +1,41 @@
 $(document).ready(function() {
-    var json_data = "js/data.json";
+  
+  var test = new XMLHttpRequest();
+  test.open('GET', "http://54.169.51.25/api/getCards", true);
+  //test.open('GET', "http://localhost:8080/api/getCards", true);
+  test.send();
+  test.onreadystatechange = processRequest
+
+  function processRequest(e) {
+    if (test.readyState == 4 ) {
+      console.log(test.statusText);
+      if (test.status == 200) {
+        var data = JSON.parse(test.response);
+        console.log(typeof data);
+        var output = '';
+        $.each(data.cards, function(k, v) {
+          console.log("card");
+          output = '<div class="row"><div class="col-md-12 text-center travscapade"><div class="user-content">';
+          output += '<img src="' + v.url + '" id="'+ v._id + '" class="travimage" data-name="travimage"/>';
+          output += '<div class="location-text"><p class="'+ v._id +'">'+ v.location +'</p></div>';
+          output += '</div><div><p class="text-center">';
+          output += '<button class="glyphicon glyphicon-heart heart button-override" aria-hidden="true" value="'+ v._id +'"></button>';
+          output += '<button class="glyphicon glyphicon-plus button-override" aria-hidden="true" id="plus"></button><br /></p></div><div id="details">';
+          output += '<a id="travel-type">' + v.interests + '</a>';
+          output += '<a id="user-photo">' + v.user_name + '</a>';
+          output += '<a><img src="' + v.user_profile_pic + '"/></a>';
+          output += '</div></div></div>';
+          $('#update').append(output);
+        });
+
+      }
+    }    
+  } 
+
+
+
+   
+    /*var json_data = "js/data.json";
 
     $.getJSON(json_data, function(data) {
         var output = '';
@@ -17,7 +53,7 @@ $(document).ready(function() {
             output += '</div></div></div>';
             $('#update').append(output);
         });
-    }); //get json data from URL
+    }); //get json data from URL*/
 
     $(document).on("click", ".heart" , function() {
       var image_id = $(this).attr("value");
