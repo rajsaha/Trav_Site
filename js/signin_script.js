9fb991ed9ab80ebfd0caa870f8d836158f8c7fd6
Hassan <hassan.ali.askari@gmail.com>
2017-06-18 20:21:30 +0600

    Sign in page design

--- js/signin_script.js
+++ js/signin_script.js
@@ -51,6 +51,28 @@ function onSignIn(googleUser) {
 }
 
 
+var googleUser = {};
+var startApp = function() {
+  gapi.load('auth2', function(){
+    // Retrieve the singleton for the GoogleAuth library and set up the client.
+    auth2 = gapi.auth2.init({
+      client_id: '244135627049-amvb3pkpavrrqthk7tlocma5lvsoc0un.apps.googleusercontent.com',
+      cookiepolicy: 'single_host_origin',
+      // Request scopes in addition to 'profile' and 'email'
+      //scope: 'additional_scope'
+    });
+    attachSignin(document.getElementById('g-login-btn'));
+  });
+};
+
+  function attachSignin(element) {
+    console.log(element.id);
+    auth2.attachClickHandler(element, {}, onSignIn, function(error) {
+          alert(JSON.stringify(error, undefined, 2));
+        });
+  }
+
+
 
 
 
@@ -62,6 +84,8 @@ $(document).ready(function() {
     console.log(id);
   });*/
 
+
+  startApp();
 
   var qParams = getUrlVars();
   console.log(qParams['state']);
